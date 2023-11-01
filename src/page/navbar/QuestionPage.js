aac296e318ef71ba6de6f4b589a9bf422f512082
SooHwanIT <ghks36912@naver.com>
2023-11-02 01:08:34 +0900

    dev QuestionPage Update

--- src/page/navbar/QuestionPage.js
+++ src/page/navbar/QuestionPage.js
@@ -12,6 +12,12 @@ const AnswerItem = ({ item }) => (
         <Text style={styles.like}>{item.like.length}</Text>
     </View>
 );
+const QuestionItem = ({ question }) => (
+    <View style={styles.questionItem}>
+        <Text style={styles.questionId}>질문 ID: {question.question_id}</Text>
+        <Text style={styles.questionContent}>질문 내용: {question.question_txt}</Text>
+    </View>
+);
 
 const QuestionPage = () => {
   const [answerBlockList,setAnswerBlockList] = useState([
@@ -72,15 +78,48 @@ const QuestionPage = () => {
 
   }, []);
     return (
+      <View>
+          <QuestionItem question={{ question_id: 1, question_txt: '질문1' }} />
         <FlatList
             data={answerBlockList}
             keyExtractor={item => item.answer_id.toString()}
             renderItem={({ item }) => <AnswerItem item={item} />}
         />
+      </View>
     );
 };
 
 const styles = StyleSheet.create({
+    questionItem:{
+        padding: 20,
+        backgroundColor:'#ffe6e0',
+        borderRadius:15,
+        marginTop:20,
+        marginHorizontal:10,
+        shadowColor: '#000', // 그림자 색상
+        shadowOffset: {
+            width: 0, // 좌우 그림자 위치
+            height: 2, // 상하 그림자 위치
+        },
+        shadowOpacity: 0.25, // 그림자 투명도
+        shadowRadius: 3.84,   // 그림자 반경
+
+        elevation: 5, // Android에만 적용되는 그림자 깊이
+    },
+    questionContent: {
+        fontSize: 18,
+        marginLeft: 20, // 왼쪽 여백 추가
+        textAlign: 'left', // 텍스트를 왼쪽 정렬
+        marginVertical:15,
+    },
+    questionId: {
+        color: '#f7b599',
+        position: 'absolute',
+        top: 10,
+        left:10,
+        fontSize: 12, // 작은 크기로 설정
+
+    },
     answerItem:{
         padding: 20,
         backgroundColor:'#FFF',
