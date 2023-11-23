c6f1ae23dea51671872a9e2293e668280d9b7bfb
효택 <user@MacBookAir-2.local>
2023-11-23 22:13:20 +0900

    AlbumPage 디자인 수정

--- src/page/navbar/AlbumPage.js
+++ src/page/navbar/AlbumPage.js
@@ -4,20 +4,25 @@ import { UserContext } from '../../../App'
 
 const Post = ({ item, navigation }) => {
     return (
-      <TouchableOpacity onPress={() => navigation.navigate('AlbumDetail', { item })}>
-          <View style={styles.post}>
-              <Image source={{ uri: item.photo_img }} style={styles.image} />
-              <View style={styles.contentContainer}>
-<View style={styles.userContainer}>
-                  <Image source={{ uri: item.profile_path }} style={styles.profileImage} />
-                  <Text style={styles.username}>{item.user_nickname}</Text>
-</View>
-                  <Text style={styles.text}>{item.photo_txt}</Text>
-              </View>
-              <View style={styles.dateContainer}>
-                  <Text style={styles.date}>{item.write_date}</Text>
-              </View>
-          </View>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('AlbumDetail', { item })}
+        activeOpacity={0.7}
+      >
+        <View style={styles.post}>
+            <View style={styles.imgContentContainer}>
+                <Image source={{ uri: item.photo_img }} style={styles.image} />
+                <View style={styles.contentContainer}>
+                    <View style={styles.userContainer}>
+                        <Image source={{ uri: item.profile_path }} style={styles.profileImage} />
+                        <Text style={styles.username}>{item.user_nickname}</Text>
+                    </View>
+                    <Text style={styles.text}>{item.photo_txt}</Text>
+                </View>
+            </View>
+            <View style={styles.dateContainer}>
+                <Text style={styles.date}>{item.write_date}</Text>
+            </View>
+        </View>
       </TouchableOpacity>
     );
 };
@@ -42,9 +47,11 @@ const AlbumPage = ({navigation}) => {
     }, [familyId]);
     return (
       <View style={styles.container}>
-          <Text style={styles.title}>가족엘범</Text>
+          <Text style={styles.title}>가족 앨범</Text>
           <FlatList
-              data={data.slice().reverse()}
+            style={styles.albumFlatlist}
+            data={data.slice().reverse()}
+            showsVerticalScrollIndicator={false}
             renderItem={({ item }) => <Post item={item} navigation={navigation} />}
             keyExtractor={item => item.photo_id.toString()}
           />
@@ -59,34 +66,52 @@ const AlbumPage = ({navigation}) => {
 
 const styles = StyleSheet.create({
     container: {
-        padding: 10,
+        width: "100%",
+        paddingBottom: 100,
         backgroundColor:'#f2f2f2',
+//        backgroundColor: 'red',
     },
     post: {
-        marginBottom: 20,
-        borderColor: '#ddd',
-        borderWidth: 1,
-        borderRadius: 10,
-        padding: 5,
-        backgroundColor:'#fff',
+        width: '100%',
+        height: 150,
         display:'flex',
         flexDirection:'row',
+//        alignItems: 'center',
+        marginBottom: 20,
+        padding: 15,
+        borderRadius: 20,
+        borderColor: '#ddd',
+        backgroundColor:'#fff',
+//        backgroundColor: 'blue',
         shadowColor: '#000', // 그림자 색상
         shadowOffset: {
             width: 0, // 좌우 그림자 위치
-            height: 2, // 상하 그림자 위치
+//            height: 2, // 상하 그림자 위치
+            height: 2,
         },
         shadowOpacity: 0.25, // 그림자 투명도
         shadowRadius: 3.84,   // 그림자 반경
-
+//        shadowRadius: 10,
         elevation: 5, // Android에만 적용되는 그림자 깊이
+    },
+    imgContentContainer: {
+        flexDirection: 'row',
+        width: '83%',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+//        backgroundColor: 'coral',
     },
     title: {
         fontSize: 24,
         fontWeight: 'bold',
         textAlign: 'center',
-        marginBottom:20,
+        marginBottom:40,
         marginTop: 20,
+    },
+    albumFlatlist: {
+        width: '100%',
+        paddingTop: 10,
+        paddingHorizontal: 10,
     },
     contentContainer: {
         flexDirection: 'column',
@@ -94,9 +119,9 @@ const styles = StyleSheet.create({
         alignItems: 'start',
         justifyContent:'flex-start',
         marginBottom: 10,
+//        backgroundColor: 'coral',
     },
     userContainer:{
-
         flexDirection: 'row',
         justifyContent:'center',
         alignItems:'center',
@@ -112,24 +137,28 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     dateContainer: {
+        alignItems: 'center',
         justifyContent: 'flex-end',
-
+        width: '17%',
+//        backgroundColor: 'cornflowerblue',
     },
     date: {
-        fontSize: 12,
+        fontSize: 10,
         color: 'gray',
     },
     image: {
-        width: 100,
-        height: 100,
+        width: 120,
+        height: 120,
 
-        borderRadius: 1,
+        marginRight: 10,
+        borderRadius: 10,
         borderColor: '#ddd',
         borderWidth: 1,
     },
     text: {
-        marginTop: 20,
-        fontSize: 16,
+        marginTop: 10,
+        fontSize: 13,
+        fontWeight: 600,
     },
     addButton: {
         zIndex:10,
@@ -140,8 +169,8 @@ const styles = StyleSheet.create({
         borderRadius: 30,
         alignItems: 'center',
         justifyContent: 'center',
-        right: 10,
-        bottom: 85,
+        right: 15,
+        bottom: 120,
         shadowColor: '#000',
         shadowOffset: {
             width: 0,
